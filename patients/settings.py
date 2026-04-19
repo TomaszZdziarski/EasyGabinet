0f6b22ad7a674308e0fbfff40628bbec71490bd7
TomaszZdziarski <tomasz.zdziarski81@gmail.com>
2026-04-19 20:50:06 +0200

    move AWS_LOCATION outside if/else block

--- patients/settings.py
+++ patients/settings.py
@@ -116,6 +116,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-north-1')
 AWS_S3_FILE_OVERWRITE = False
+AWS_LOCATION = 'media'    # MOVE HERE — outside if/else
 
 if DEBUG:
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
@@ -123,7 +124,6 @@ if DEBUG:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    AWS_LOCATION = 'media'
     MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
