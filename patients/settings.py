c5f68658670f803c868fe92cdb4191bec603280b
TomaszZdziarski <tomasz.zdziarski81@gmail.com>
2026-04-19 22:15:57 +0200

    fix upload_to media/ on all image fields

--- patients/settings.py
+++ patients/settings.py
@@ -126,7 +126,8 @@ if DEBUG:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/media/'
+    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
