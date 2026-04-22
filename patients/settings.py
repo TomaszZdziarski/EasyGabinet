0a4034927e297fb7cb0057650b915835b5f2f0db
TomaszZdziarski <tomasz.zdziarski81@gmail.com>
2026-04-22 19:23:43 +0200

    fix: remove AWS_LOCATION to fix double media path

--- patients/settings.py
+++ patients/settings.py
@@ -118,7 +118,7 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-north-1')
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_AUTH = False
-AWS_LOCATION = 'media'
+#AWS_LOCATION = 'media'
 
 if DEBUG:
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
@@ -126,7 +126,7 @@ if DEBUG:
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 else:
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/media/'
+    MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
     # added /media/ at the end
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
