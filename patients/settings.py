d290afacd76ea2358267340294031850d6626a9d
TomaszZdziarski <tomasz.zdziarski81@gmail.com>
2026-04-22 20:11:17 +0200

    fix: always use S3 storage

--- patients/settings.py
+++ patients/settings.py
@@ -123,6 +123,7 @@ AWS_DEFAULT_ACL = None
 AWS_QUERYSTRING_AUTH = False
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+print(f"DEFAULT_FILE_STORAGE SET TO: {DEFAULT_FILE_STORAGE}")
 MEDIA_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
