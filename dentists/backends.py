fc7172f2b4eafe7b96c5dbfcab0c1d825c2fb9f1
TomaszZdziarski <tomasz.zdziarski81@gmail.com>
2026-04-30 14:12:10 +0200

    Fix EmailBackend to accept username field for admin login

--- dentists/backends.py
+++ dentists/backends.py
@@ -1,17 +1,14 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
-
-# FILE TO FIX THE BUG WITH AUTH WHEN PROJECT DEPLOYED
 User = get_user_model()
 
 class EmailBackend(ModelBackend):
-    def authenticate(self, request, email=None, password=None, **kwargs):
+    def authenticate(self, request, username=None, email=None, password=None, **kwargs):
         try:
-            user = User.objects.get(email=email)
+            user = User.objects.get(email=email or username)
         except User.DoesNotExist:
             return None
-
         if user.check_password(password):
             return user
         return None
