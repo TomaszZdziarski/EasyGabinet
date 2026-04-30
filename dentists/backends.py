e941130535b1de6d81e51d50e4f21e1827e51772
TomaszZdziarski <tomasz.zdziarski81@gmail.com>
2026-04-30 14:31:56 +0200

    Add debug logging to EmailBackend

--- dentists/backends.py
+++ dentists/backends.py
@@ -1,14 +1,21 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
+import logging
 
+logger = logging.getLogger(__name__)
 User = get_user_model()
 
 class EmailBackend(ModelBackend):
     def authenticate(self, request, username=None, email=None, password=None, **kwargs):
+        logger.warning(f"EmailBackend called with username={username}, email={email}")
         try:
             user = User.objects.get(email=email or username)
-        except User.DoesNotExist:
+            logger.warning(f"User found: {user.email}, checking password...")
+            if user.check_password(password):
+                logger.warning("Password correct!")
+                return user
+            logger.warning("Password incorrect!")
             return None
-        if user.check_password(password):
-            return user
-        return None
+        except User.DoesNotExist:
+            logger.warning("User not found!")
+            return None
